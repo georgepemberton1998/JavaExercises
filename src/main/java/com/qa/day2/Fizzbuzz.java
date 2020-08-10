9122d78a0fde27d8394307692736d51d353fce87
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 15:28:45 +0100

    Changed the message that is printed out

--- src/main/java/com/qa/day2/Fizzbuzz.java
+++ src/main/java/com/qa/day2/Fizzbuzz.java
@@ -3,7 +3,29 @@ package com.qa.day2;
 public class Fizzbuzz {
     public static void main( String[] args) {
 
-        System.out.println(5 + 5);
+        int number1 = 5;
+        int number2 = 7;
+        int result = number1 + number2;
+            System.out.println(result);
+
+        int number3 = 4;
+        int number4 = 5;
+        int result2 = number3 * number4;
+        System.out.println(result2);
+
+        int number5 = 10;
+        int number6 = 2;
+        int result3 = number5 - number6;
+        System.out.println(result3);
+
+        int number7 = 10;
+        int number8 = 4;
+        double result4 = number7 / number8;
+        System.out.println(result4);
+
+
+
+
 
     }
 }

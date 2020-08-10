ec79b225c9d4b79f2d9475e5f4419f251e5b1fcb
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 19:06:15 +0100

    Completed all events and functions

--- src/main/java/com/qa/day2/Flowchart2.java
+++ src/main/java/com/qa/day2/Flowchart2.java
@@ -2,18 +2,38 @@ package com.qa.day2;
 
 public class Flowchart2 {
     public static void main(String [] args) {
-        flow(3000);
+        flow(50);
     }
         public static void flow(int num1){
         if(num1>2000){
             System.out.println("A");
+            if(num1<6000){
+                System.out.println("C");
+            }else {
+                System.out.println("B");
+                if(num1>=4000){
+                    System.out.println("D");
+                }else {
+                    System.out.println("E");
+                }
+            }
+        }else {
+            System.out.println("1");
             if(num1>100){
                 System.out.println("2");
             }else {
                 System.out.println("3");
+                if(num1>600){
+                    System.out.println("5");
+                }else {
+                    System.out.println("4");
+                    if(num1>500) {
+                        System.out.println("6");
+                    }else {
+                        System.out.println("7");
+                    }
+                }
             }
-        }else {
-            System.out.println("1");
         }
     }
 }

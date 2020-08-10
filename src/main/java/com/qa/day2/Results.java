aeda68c165d78077c6fc1ffb7e47d9ca988bcb80
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 18:24:31 +0100

    Changes made as best as possible - functions poorly. Return to this tomorrow.

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -5,20 +5,27 @@ import java.sql.SQLOutput;
 public class Results {
 
     public static void main( String[] args) {
-        results1(100, 100, 100);
-        percentage1(50);
+        results(100, 100, 100);
+        percentage1(60);
+        fail()
 
     }
-    public static int results1 (int Physics, int Chemistry, int Biology){
+    public static int results (int Physics, int Chemistry, int Biology){
         int total = Physics + Chemistry + Biology;
         System.out.println("Congratulations, you scored: " + Physics + "/150 in your Physics test");
         System.out.println("Congratulations, you scored: " + Chemistry + "/150 in your Chemistry test");
         System.out.println("Congratulations, you scored: " + Biology + "/150 in your Biology test");
         System.out.println("Congratulations, you scored: " + total + "/450 in total");
-        return total;
+        return 0;
     }
     public static int percentage1 (int percentage){
         System.out.println("Congratulations, your percent is: " + percentage + "%");
         return 0;
     }
+    public static int fail ({
+        if pertage
+        System.out.println("Congratulations, your percent is: " + percentage + "%");
+        return 0;
+    }
+
 }

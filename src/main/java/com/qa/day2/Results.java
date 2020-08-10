d0ba0df78bbd5c11a6f82adb2b7654de52b26815
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 17:57:34 +0100

    Optimized Total section with added variable

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -1,18 +1,24 @@
 package com.qa.day2;
 
+import java.sql.SQLOutput;
+
 public class Results {
 
     public static void main( String[] args) {
-        results1(100, 100, 100, 300, 60);
+        results1(100, 100, 100);
+        percentage1(50);
 
     }
-    public static int results1 (int Physics, int Chemistry, int Biology, int Total, int Percentage){
-
+    public static int results1 (int Physics, int Chemistry, int Biology){
+        int total = Physics + Chemistry + Biology;
         System.out.println("Congratulations, you scored: " + Physics + "/150 in your Physics test");
         System.out.println("Congratulations, you scored: " + Chemistry + "/150 in your Chemistry test");
         System.out.println("Congratulations, you scored: " + Biology + "/150 in your Biology test");
-        System.out.println("Congratulations, you scored: " + Total + "/450 in total");
-        System.out.println("Congratulations, your percentage is:" + Percentage );
+        System.out.println("Congratulations, you scored: " + total + "/450 in total");
+        return total;
+    }
+    public static int percentage1 (int percentage){
+        System.out.println("Congratulations, your percent is: " + percentage + "%");
         return 0;
     }
 }

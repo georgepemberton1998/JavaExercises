0830e878f900bff0c5129218c8a39ff517ae8bd1
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 16:23:45 +0100

    Added multiplication

--- src/main/java/com/qa/day2/Fizzbuzz.java
+++ src/main/java/com/qa/day2/Fizzbuzz.java
@@ -5,6 +5,7 @@ public class Fizzbuzz {
     public static void main( String[] args) {
         addition(2, 5);
         subtraction(10, 5);
+        multiplication(3, 7);
     }
 
     public static int addition (int num1 , int num2){
@@ -16,6 +17,12 @@ public class Fizzbuzz {
     public static int subtraction (int num1 , int num2){
 
         int result = num1 - num2;
+        System.out.println(result);
+        return result;
+    }
+    public static int multiplication(int num1 , int num2){
+
+        int result = num1 * num2;
         System.out.println(result);
         return result;
     }

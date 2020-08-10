2da52873bd833a6cf393ecdf855d6226154d9d98
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 19:12:33 +0100

    Made a start but no real progress

---
+++ src/main/java/com/qa/day2/Blackjack.java
@@ -0,0 +1,18 @@
+package com.qa.day2;
+
+public class Blackjack {
+    public static void main(String [] args) {
+        int hand1(1, 15)
+        int hand2(10, 10)
+        int hand3(15, 15)
+        int hand4(11, 10)
+
+    public static int play(int hand1){
+
+
+
+
+
+        }
+    }
+}

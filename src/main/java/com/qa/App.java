dbb23f0e015293f7d376556e7e9d8977555720cb
George Pemberton <georgepemberton1998@gmail.com>
2020-08-10 16:22:31 +0100

    Made the correct code for addition and subtraction

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -1,14 +1,2 @@
 package com.qa;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-
-    {
-        System.out.println( "Hello Everyone!" );
-    }
-}
--- src/main/java/com/qa/day2/Fizzbuzz.java
+++ src/main/java/com/qa/day2/Fizzbuzz.java
@@ -1,31 +1,22 @@
 package com.qa.day2;
 
 public class Fizzbuzz {
+
     public static void main( String[] args) {
+        addition(2, 5);
+        subtraction(10, 5);
+    }
 
-        int number1 = 5;
-        int number2 = 7;
-        int result = number1 + number2;
-            System.out.println(result);
+    public static int addition (int num1 , int num2){
 
-        int number3 = 4;
-        int number4 = 5;
-        int result2 = number3 * number4;
-        System.out.println(result2);
+       int result = num1 + num2;
+       System.out.println(result);
+       return result;
+    }
+    public static int subtraction (int num1 , int num2){
 
-        int number5 = 10;
-        int number6 = 2;
-        int result3 = number5 - number6;
-        System.out.println(result3);
-
-        int number7 = 10;
-        int number8 = 4;
-        double result4 = number7 / number8;
-        System.out.println(result4);
-
-
-
-
-
+        int result = num1 - num2;
+        System.out.println(result);
+        return result;
     }
 }
